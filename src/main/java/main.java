ae2733ee0e517fc218cb91d81eba09a1e2bf1f76
Student   ROBERTS Lucas <04899@farnborough.ac.uk>
2020-11-06 11:03:10 +0000

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+
+        String word1 = ("Hello world!");
+
+        System.out.println(word1);
+
+        System.out.println(word1.length());
+
+
+    }
+
+}
